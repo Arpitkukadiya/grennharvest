1c802c9cf2300a0bb2ca44470a7b4913c1fff4bd
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-11-11 09:10:02 +0530

    Update crop_info.php

--- crop_info.php
+++ crop_info.php
@@ -25,7 +25,7 @@ $crops = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Available Crops</title>
+    <title>Available Crops.</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body {
