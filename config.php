00841f8d2afe00432e0869f3d440cce32b9bb70f
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-11-12 09:14:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $username = 'root';
 $password = '';
 
 try {
+    
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
